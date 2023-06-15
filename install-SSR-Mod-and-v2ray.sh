928d23b346c42296238e6d339ec83bdec027335e
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-06-15 11:40:19 +0800

    Update install-SSR-Mod-and-v2ray.sh

--- install-SSR-Mod-and-v2ray.sh
+++ install-SSR-Mod-and-v2ray.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# 获取节点ID，超时时间为 15 秒
+# 获取节点ID，超时时间为 10 秒
 valid_input=false
-wait=15
+wait=10
 while [ "$valid_input" = false ]; do
   echo "请输入节点ID（默认为 0 ）："
   if read -t $wait node_id; then
@@ -17,7 +17,7 @@ while [ "$valid_input" = false ]; do
   if [[ "$node_id" =~ ^[0-9]+$ ]]; then
     valid_input=true
   else
-    wait=15
+    wait=10
     echo "输入无效，请重新输入一个有效的数字！"
   fi
 done
