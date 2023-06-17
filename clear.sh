a9ac2e93887886e9a3916ae397ce19ec230f96a9
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-06-17 10:18:34 +0800

    Create clear.sh

---
+++ clear.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#删除ssr-mod+libsodium
+shred -zvu -n 5 /root/shadowsocks-mod/userapiconfig.py
+rm -rf /root/shadowsocks-mod
+rm -rf /root/libsodium*
+rm -rf /root/bbr.sh
+rm -rf /root/install_bbr.log
+
+#删除V2ray
+shred -zvu -n 5 /usr/local/etc/v2ray/config.json
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove
+rm -rf /usr/local/etc/v2ray
+rm -rf /var/log/v2ray
+
+#删除定时任务
+crontab -r
+
+#删除命令历史
+rm -rf ~/.bash_history
+history -c
+
+#重启
+echo "是否重启以彻底删除数据？输入N或n不重启，否则为重启："
+if read -t 10 isDo; then
+  if [ "$isDo" != "N" ] && [ "$isDo" != "n" ]; then
+	echo "正在重启..."
+    reboot
+  else
+	echo "取消重启"
+  fi
+else
+  echo "正在重启..."
+  reboot
+fi
+
