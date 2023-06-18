e9c1b609e416810491fd5cc09fc0ba9ce5953437
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-06-18 18:25:37 +0800

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -3,25 +3,6 @@
 # 删除脚本自身
 rm -- "$0"
 
-# 清空命令历史
-clear_command_history() {
-  history -c
-  rm -rf "$HISTFILE"
-  history -w
-  history -c
-}
-
-
-# 删除执行清除脚本的命令历史记录
-delete_clear_script_command_history() {
-  local clear_script_command="wget https://github.com/dgou45/fhs-install-v2ray/raw/ssr/clear.sh && chmod +x clear.sh && ./clear.sh"
-  local line_number=$(history | grep -n "$clear_script_command" | awk '{print $1}' FS=":")
-  
-  if [ -n "$line_number" ]; then
-    history -d "$line_number"
-  fi
-}
-
 # 重启
 echo "是否重启以彻底删除数据？输入N或n不重启，否则为重启："
 read -t 10 isDo
@@ -50,12 +31,10 @@ history -c
 if [ "$isDo" != "N" ] && [ "$isDo" != "n" ]; then
     echo "正在重启..."
     echo "所有命令执行成功"
-    clear_command_history
     reboot
 else
     echo "取消重启"
     echo "所有命令执行成功"
-    clear_command_history
 fi
 
 
