5d963e0d0fa5dc9176977ceda6b355b423f6559e
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-06-27 16:19:10 +0800

    Update update-ssr-obfs.sh

--- update/update-ssr-obfs.sh
+++ update/update-ssr-obfs.sh
@@ -19,17 +19,17 @@ tcprelay02="/root/tcprelay02.py"
 tcprelay="/root/shadowsocks-mod/shadowsocks/tcprelay.py"
 
 
-# 比较 tcprelay 是否相同
-if cmp -s "$tcprelay01" "$tcprelay"; then
-    echo "与 tcprelay01 相同，开启替换"
+# 比较 web_transfer 和 tcprelay 是否相同
+if cmp -s "$web_transfer" "$web_transfer01" && cmp -s "$tcprelay" "$tcprelay01"; then
+    echo "web_transfer 与 web_transfer01 相同，且tcprelay 与 tcprelay01 相同，开启替换"
     wget -O /root/shadowsocks-mod/web_transfer.py https://github.com/dgou45/fhs-install-v2ray/raw/ssr/update/web_transfer1.py
     wget -O /root/shadowsocks-mod/shadowsocks/tcprelay.py https://github.com/dgou45/fhs-install-v2ray/raw/ssr/update/tcprelay1.py
-elif cmp -s "$tcprelay02" "$tcprelay"; then
-    echo "与 tcprelay02 相同，开启替换"
+elif cmp -s "$web_transfer" "$web_transfer02" && cmp -s "$tcprelay" "$tcprelay02"; then
+    echo "web_transfer 与 web_transfer02 相同，且tcprelay 与 tcprelay02 相同，开启替换"
     wget -O /root/shadowsocks-mod/web_transfer.py https://github.com/dgou45/fhs-install-v2ray/raw/ssr/update/web_transfer2.py
     wget -O /root/shadowsocks-mod/shadowsocks/tcprelay.py https://github.com/dgou45/fhs-install-v2ray/raw/ssr/update/tcprelay2.py
 else
-    echo "tcprelay 与任何类型都不相同"
+    echo "web_transfer 或 tcprelay 与任何类型都不相同"
     exit 0
 fi
 
