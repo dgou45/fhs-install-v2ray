9fe7ac454c156349d01ee1b5999f3d0c4bc0086a
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-07-29 20:58:08 +0800

    Update cloudflare-ddns.sh

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -23,15 +23,6 @@ sudo sed -i "s/^#*record_name=.*/record_name= $domain/" /usr/local/bin/cf-ddns.s
 # 新增定时任务
 (crontab -l ; echo "* * * * * /usr/local/bin/cf-ddns.sh >/dev/null 2>&1") | crontab -
 
-# 执行 cf-ddns.sh 脚本并将输出保存到变量 output 中
-output=$(bash /usr/local/bin/cf-ddns.sh)
-
-# 检查脚本是否正常运行
-if [[ $output =~ "IP changed to" ]]; then
-    echo "脚本运行成功，IP已经解析成功！"
-else
-    echo "脚本运行失败，请检查！"
-fi
 
 
 
