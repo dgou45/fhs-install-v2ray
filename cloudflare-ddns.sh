ecb007fcd5351bc068cfd29fe7b0fb8828c6ea69
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-08-02 09:23:38 +0800

    Update cloudflare-ddns.sh

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -23,7 +23,9 @@ sudo sed -i "s|record_name=\"www.example.com\"|record_name=\"$domain\"|" /usr/lo
 # 新增定时任务
 (crontab -l ; echo "* * * * * /usr/local/bin/cf-ddns.sh >/dev/null 2>&1") | crontab -
 
-
+echo
+echo
+echo -e "\033[32m所有命令执行成功！\033[0m"
 
 
 
