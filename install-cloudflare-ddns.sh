44e547640835354dd60582efff4a06e073e651e5
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-08-13 14:46:50 +0800

    Update install-cloudflare-ddns.sh

--- install-cloudflare-ddns.sh
+++ install-cloudflare-ddns.sh
@@ -22,19 +22,19 @@ sudo sed -i "s|record_name=\"www.example.com\"|record_name=\"$domain\"|" /usr/lo
 
 # 修改auth
 if [ -n "$1" ]; then
-	sudo sed -i "s|\"user@example.com\"|$1|" /usr/local/bin/cf-ddns.sh
+	sudo sed -i "s|auth_email=\"user@example.com\"|auth_email=$1|" /usr/local/bin/cf-ddns.sh
 else
 	echo -e "\033[31m没有获取到参数1，请手动修改！\033[0m"
 fi
 
 if [ -n "$2" ]; then
-	sudo sed -i "s|\"c2547eb745079dac9320b638f5e225cf483cc5cfdda41\"|$2|" /usr/local/bin/cf-ddns.sh
+	sudo sed -i "s|auth_key=\"c2547eb745079dac9320b638f5e225cf483cc5cfdda41\"|auth_key=$2|" /usr/local/bin/cf-ddns.sh
 else
 	echo -e "\033[31m没有获取到参数2，请手动修改！\033[0m"
 fi
 
 if [ -n "$3" ]; then
-	sudo sed -i "s|\"example.com\"|$3|" /usr/local/bin/cf-ddns.sh
+	sudo sed -i "s|zone_name=\"example.com\"|zone_name=$3|" /usr/local/bin/cf-ddns.sh
 else
 	echo -e "\033[31m没有获取到参数3，请手动修改！\033[0m"
 fi
@@ -45,9 +45,7 @@ bash /usr/local/bin/cf-ddns.sh
 # 新增定时任务
 (crontab -l ; echo "* * * * * /usr/local/bin/cf-ddns.sh >/dev/null 2>&1") | crontab -
 
-echo
-echo
-echo -e "\033[32m所有命令执行成功！\033[0m"
+echo -e "\033[32m恭喜您，\033[33m所有命令执行成功！\033[0m"
 
 
 
