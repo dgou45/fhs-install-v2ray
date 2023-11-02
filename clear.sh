5833d093a0c248484aff684bb869890db4ca95ef
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-11-02 10:15:28 +0800

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -23,9 +23,9 @@ rm -rf /root/cloudflare.ids  cloudflare.log  ip.txt
 # 删除定时任务
 crontab -r
 
-echo "... ..."
-echo "... ..."
-echo "... ..."
+echo "......"
+echo "......"
+echo "......"
 echo -e "\033[32m恭喜您，\033[33m$(basename "$0") 的所有命令都已经执行成功！\033[0m"
 
 
